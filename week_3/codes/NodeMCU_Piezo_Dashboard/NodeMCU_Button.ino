a36b016e1d1ad915f194f7d8f1cc3cf7d493717a
Nonpawit Ekburanawat <nonpawit.ek@gmail.com>
2023-04-04 02:30:45 +0700

    In-class work example 2

---
+++ week_3/codes/NodeMCU_Piezo_Dashboard/NodeMCU_Button.ino
@@ -0,0 +1,90 @@
+// Import essential libraries
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+// Config WiFi
+const char* ssid          = "__SSID__";
+const char* password      = "__PASS__";
+
+// Config NETPIE
+const char* mqtt_server   = "broker.netpie.io";
+const int   mqtt_port     = 1883;
+const char* mqtt_client   = "__CLIENT_ID__";
+const char* mqtt_username = "__TOKEN__";
+const char* mqtt_password = "__SECRET__";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() 
+{
+  while (!client.connected()) 
+  {
+    if (client.connect(mqtt_client, mqtt_username, mqtt_password)) 
+    {
+      Serial.println("connected");
+      client.subscribe("@msg/#");
+    } 
+    else 
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      delay(5000);
+    } 
+  } 
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) 
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if ((String)topic == "@msg/button" && (char)payload[0] == '1')
+  {
+    Serial.println("Button Pressed");
+  }
+}
+
+void setup() 
+{
+  // Initialize sensors and serial
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  // Initialize WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize with MQTT
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() 
+{
+  // Check connection status
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+}
